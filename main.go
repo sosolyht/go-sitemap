e2384e7fe03fe2ba1698f7f91cbc6f6f0b7d3c59
sosolyht <sosolyht@gmail.com>
2023-04-18 12:21:50 +0900

    feat: change frequence method

--- main.go
+++ main.go
@@ -6,11 +6,10 @@ import (
 )
 
 func main() {
-	res, _ := sitemap.NewURL().AddURL(sitemap.URL{
-		Loc:      "http://google.com",
-		LastMod:  "2023-04-18",
-		Freq:     "monthly",
-		Priority: 0.5,
+	err := sitemap.NewURL().AddURL(sitemap.URL{
+		Loc:        "https://google.com",
+		ChangeFreq: sitemap.MONTHLY,
+		Priority:   0.5,
 	})
-	fmt.Println(string(res))
+	fmt.Println(err)
 }
--- sitemap/sitemap.go
+++ sitemap/sitemap.go
@@ -3,49 +3,105 @@ package sitemap
 import (
 	"encoding/xml"
 	"os"
+	"time"
 )
 
 const (
-	XmlVersion = "<?xml version=\"1.0″ encoding=\"UTF-8″?>"
-	URLSet     = "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9″>"
+	XmlVersion   = `<?xml version="1.0" encoding="UTF-8"?>`
+	SitemapIndex = `<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9">`
+)
+
+type ChangeFrequency string
+
+const (
+	ALWAYS  ChangeFrequency = "always"
+	HOURLY  ChangeFrequency = "hourly"
+	DAILY   ChangeFrequency = "daily"
+	WEEKLY  ChangeFrequency = "weekly"
+	MONTHLY ChangeFrequency = "monthly"
+	YEARLY  ChangeFrequency = "yearly"
+	NEVER   ChangeFrequency = "never"
 )
 
 type Sitemap struct {
-	XMLVersion string `xml:"XMLVersion,omitempty"`
-	URLSet     string `xml:"URLSet,omitempty"`
-	URL        []URL  `xml:"url,omitempty"`
+	XMLVersion   string `xml:"xmlVersion,omitempty"`
+	SitemapIndex string `xml:"sitemapindex,omitempty"`
+	URL          []URL  `xml:"url,omitempty"`
 }
 
 type URL struct {
-	Loc      string  `xml:"loc"`
-	LastMod  string  `xml:"lastmod"`
-	Freq     string  `xml:"changefreq"`
-	Priority float32 `xml:"priority"`
+	Loc        string          `xml:"loc"`
+	LastMod    string          `xml:"lastmod"`
+	ChangeFreq ChangeFrequency `xml:"changefreq"`
+	Priority   float32         `xml:"priority"`
 }
 
 func NewURL() *Sitemap {
 	return &Sitemap{
-		XMLVersion: XmlVersion,
-		URLSet:     URLSet,
+		XMLVersion:   XmlVersion,
+		SitemapIndex: SitemapIndex,
 	}
 }
 
-func (s *Sitemap) AddURL(url URL) ([]byte, error) {
+func (s *Sitemap) AddURL(url URL) error {
+	s.LastModify()
 	s.URL = append(s.URL, url)
 
-	resp, err := xml.MarshalIndent(s.URL, "", " ")
+	resp, err := xml.MarshalIndent(s.URL, "", "   ")
 	if err != nil {
 		panic(err)
 	}
 
-	m, err := os.Create("sitemap.xml")
+	sm, err := os.Create("sitemaps/sitemap.xml")
 	if err != nil {
 		panic(err)
 	}
 
-	defer m.Close()
+	defer sm.Close()
 
-	m.Write(resp)
+	sm.Write(resp)
 
-	return resp, err
+	return err
+}
+
+func (s *Sitemap) LastModify() {
+	var url URL
+	timeLayout := "2006-01-02"
+	dateString := time.Now().Format(timeLayout)
+	url.LastMod = dateString
+}
+
+func (s *Sitemap) FrequencyAlways() {
+	var url URL
+	url.ChangeFreq = ALWAYS
+}
+
+func (s *Sitemap) FrequencyHourly() {
+	var url URL
+	url.ChangeFreq = HOURLY
+}
+
+func (s *Sitemap) FrequencyDaily() {
+	var url URL
+	url.ChangeFreq = DAILY
+}
+
+func (s *Sitemap) FrequencyWeekly() {
+	var url URL
+	url.ChangeFreq = WEEKLY
+}
+
+func (s *Sitemap) FrequencyMonthly() {
+	var url URL
+	url.ChangeFreq = MONTHLY
+}
+
+func (s *Sitemap) FrequencyYearly() {
+	var url URL
+	url.ChangeFreq = YEARLY
+}
+
+func (s *Sitemap) FrequencyNever() {
+	var url URL
+	url.ChangeFreq = NEVER
 }
